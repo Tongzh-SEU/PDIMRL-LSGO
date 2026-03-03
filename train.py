90f5bccbfb4f3ab329bad8fcb6f027cc36505bb0
Tongzh <toooooongx2@gmail.com>
2026-03-04 02:13:14 +0800

    Update train.py

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ def train():
 
     has_continuous_action_space = True  # continuous action space; else discrete
 
-    max_ep_len = 14  # max timesteps in one episode
+    max_ep_len = 14  # max timesteps in one episode (should = action num in the episodes, need to fix)
     max_training_timesteps = int(3e6)  # break training loop if timeteps > max_training_timesteps
 
     print_freq = max_ep_len * 20  # print avg reward in the interval (in num timesteps)
